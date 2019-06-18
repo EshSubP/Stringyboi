1e4f48455ba03b43a8fd1b6fb7824a6a950429e4
EshSubP <51940674+EshSubP@users.noreply.github.com>
2019-06-18 15:59:41 +0530

    Add files via upload

---
+++ mp6.py
@@ -0,0 +1,16 @@
+lst = []
+n = int(input("Enter number of elements: ")) 
+for i in range(n): 
+    ele = tuple(input("Enter the tuple "))
+    lst.append(ele)  
+    print(lst)
+        
+for x in lst:
+    for y in x:
+        ys=y+'='
+        print(strip.ys('='))
+    print(';',end="")  
+
+
+        
+    
